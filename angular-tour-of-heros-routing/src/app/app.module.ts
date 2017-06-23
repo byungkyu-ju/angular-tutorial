d407134fbee6a36ce2832b3f2718b29e5876b5c5
Byungkyu Ju <noel1227@gmail.com>
2017-06-23 09:15:40 +0900

    routing

--- angular-tour-of-heros-routing/src/app/app.module.ts
+++ angular-tour-of-heros-routing/src/app/app.module.ts
@@ -1,16 +1,25 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {NgModule} from "@angular/core";
+import {BrowserModule} from "@angular/platform-browser";
+import {FormsModule} from "@angular/forms";
 
-import { AppComponent } from './app.component';
+import {AppComponent} from "./app.component";
+import {HeroDetailComponent} from "./hero-detail.component";
+import {HeroesComponent} from "./heroes.component";
+import {HeroService} from "./hero.service";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule //NgModule 사용을 위함
   ],
-  providers: [],
+  declarations: [
+    AppComponent,
+    HeroDetailComponent,
+    HeroesComponent
+  ],
+  providers: [
+    HeroService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
